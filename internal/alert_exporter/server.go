0f92bb06a8346a4af6f4ab295f7e0525cd97d28a
Avishay Traeger <atraeger@redhat.com>
2025-06-16 15:09:56 +0300

    fixes

--- internal/alert_exporter/server.go
+++ internal/alert_exporter/server.go
@@ -8,12 +8,7 @@ import (
 	"time"
 
 	"github.com/flightctl/flightctl/internal/config"
-	"github.com/flightctl/flightctl/internal/consts"
-	"github.com/flightctl/flightctl/internal/kvstore"
 	"github.com/flightctl/flightctl/internal/service"
-	"github.com/flightctl/flightctl/internal/store"
-	"github.com/flightctl/flightctl/internal/tasks_client"
-	"github.com/flightctl/flightctl/pkg/queues"
 	"github.com/sirupsen/logrus"
 )
 
@@ -33,42 +28,11 @@ func New(
 	}
 }
 
-func (s *Server) Run() error {
-	ctx, cancel := context.WithCancel(context.Background())
-	ctx = context.WithValue(ctx, consts.EventSourceComponentCtxKey, "flightctl-alert-exporter")
-	ctx = context.WithValue(ctx, consts.EventActorCtxKey, "service:flightctl-alert-exporter")
+func (s *Server) Run(ctx context.Context, serviceHandler service.Service) error {
+	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	s.log.Println("Initializing data store")
-	db, err := store.InitDB(s.cfg, s.log)
-	if err != nil {
-		s.log.Fatalf("initializing data store: %v", err)
-	}
-
-	store := store.NewStore(db, s.log.WithField("pkg", "store"))
-	defer store.Close()
-
-	queuesProvider, err := queues.NewRedisProvider(context.Background(), s.log, s.cfg.KV.Hostname, s.cfg.KV.Port, s.cfg.KV.Password)
-	if err != nil {
-		return err
-	}
-	defer queuesProvider.Stop()
-
-	kvStore, err := kvstore.NewKVStore(ctx, s.log, s.cfg.KV.Hostname, s.cfg.KV.Port, s.cfg.KV.Password)
-	if err != nil {
-		return err
-	}
-
-	publisher, err := tasks_client.TaskQueuePublisher(queuesProvider)
-	if err != nil {
-		return err
-	}
-	callbackManager := tasks_client.NewCallbackManager(publisher, s.log)
-	serviceHandler := service.NewServiceHandler(store, callbackManager, kvStore, nil, s.log, "", "")
-
 	alertExporter := NewAlertExporter(s.log, serviceHandler, s.cfg)
-
-	// Signal handling
 
 	go func() {
 		sigCh := make(chan os.Signal, 1)
